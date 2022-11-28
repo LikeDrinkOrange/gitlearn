fa2cc094717aa886a5c29ffa006df7d18326e4f0
lg245 <lg2458678137@qq.com>
2022-11-28 20:09:54 +0800

    the frist commmit

---
+++ src/main/java/edu/jxut/GitTest.java
@@ -0,0 +1,20 @@
+package edu.jxut;/**
+ * @description TODO
+ * @author lg245
+ * @date 2022/11/28 19:59
+ */
+
+/**
+ * @ClassName GitTest
+ * @Description
+ * @Author lg245
+ * @Date 2022/11/28 19:59
+ * @Version 1.0
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
