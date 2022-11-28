ef1735aacb62116fd893dff8fbf3d610dec33a03
lg245 <lg2458678137@qq.com>
2022-11-28 20:12:33 +0800

    the third commmit

--- src/main/java/edu/jxut/GitTest.java
+++ src/main/java/edu/jxut/GitTest.java
@@ -1,23 +1,9 @@
-package edu.jxut;/**
- * @description TODO
- * @author lg245
- * @date 2022/11/28 19:59
- */
-
-import org.omg.Messaging.SyncScopeHelper;
-
-/**
- * @ClassName GitTest
- * @Description
- * @Author lg245
- * @Date 2022/11/28 19:59
- * @Version 1.0
- */
-
+package edu.jxut;
 
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("helloworld1");
+        System.out.println("helloworld2");
     }
 }
